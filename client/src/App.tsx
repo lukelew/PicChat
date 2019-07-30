c762fb74eafd742b423156e7b513ee57d692f66b
Luke_Lu <ailuqun313@gmail.com>
2019-07-30 13:54:30 +1000

    The structur of whole directories changed
    The whole dirctories of front-end has been moved to 'client' directory with its own package.json. The top level has been left for server end.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
