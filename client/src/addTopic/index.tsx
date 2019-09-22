883deacfc2a96b8cf03d699d72e6df43b49297a5
Judrcinho <evgeniya.yurieva@gmail.com>
2019-09-22 16:07:50 +1000

    post topic - authorisation error is occuring

--- client/src/addTopic/index.tsx
+++ client/src/addTopic/index.tsx
@@ -2,32 +2,34 @@ import React from "react";
 import {Modal, Upload, message, Button, Icon} from 'antd';
 import './index.scss';
 
-const props = {
-    name: 'image',
-    action: process.env.REACT_APP_API_URL + '/images/upload',
-    onChange(info: any) {
+interface IAddTopicState {
+    visible: boolean;
+    img_url: string;
+}
+
+class AddTopic extends React.PureComponent<{}, IAddTopicState> {
+    upload_props: any;
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            visible: false,
+            img_url: ""
+          }
+        }
+
+    onImageUpload(info: any) {
       if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
       }
       if (info.file.status === 'done') {
+        this.setState({
+          img_url: info.file.response
+        });
         message.success(`File uploaded successfully!`);
       } else if (info.file.status == 'error') {
         message.error(`The image contains text/unappropriate content and can't be uploaded.`);
       }
-    },
-  };
-
-interface IAddTopicState {
-    visible: boolean;
-}
-
-class AddTopic extends React.PureComponent<{}, IAddTopicState> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            visible: false
-        }
-    }
+    };
 
     showModal = () => {
         this.setState({
@@ -35,8 +37,23 @@ class AddTopic extends React.PureComponent<{}, IAddTopicState> {
         });
       };
   
-      handleOk = (e: any) => {
-        console.log(e);
+      postTopic = () => {
+        let url = process.env.REACT_APP_API_URL +'/topics';
+        let post_data = {picUrl: this.state.img_url};
+        console.log("post topic with url: " + this.state.img_url);
+
+        fetch(url,{
+          method:'POST',
+          body: JSON.stringify(post_data),
+          headers: new Headers({
+              'Content-Type': 'application/json'
+          })
+      }).then(res=>res.json()).then(
+          data=>{
+            console.log(data);
+          }
+      )
+
         this.setState({
           visible: false,
         });
@@ -61,11 +78,13 @@ class AddTopic extends React.PureComponent<{}, IAddTopicState> {
               title="Upload new picture to start new topic"
               visible={this.state.visible}
               okText="Create new topic"
-              onOk={this.handleOk}
+              onOk={() => {this.postTopic()}}
               onCancel={this.handleCancel}
             >
                 <div className="image_upload">
-                <Upload {...props}>
+                <Upload name="image"
+                        action={process.env.REACT_APP_API_URL + '/images/upload'}
+                        onChange={(info) => {this.onImageUpload(info);}}>
                     <Button>
                     <Icon type="upload" /> Click to upload image
                     </Button>
