fb79ced2a12802f9f15f209a8e317a1ae809fe2c
Luke Lu <ailuqun313@gmail.com>
2019-10-02 00:31:47 +1000

    test for deploy to heroku

---
+++ client/build/precache-manifest.9dcbe047b92c225f1b7624123bf272e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609f56e212c6005d4fa7ab86dad07380",
+    "url": "/index.html"
+  },
+  {
+    "revision": "338954f390a5c73ad0e2",
+    "url": "/static/css/2.91d7f120.chunk.css"
+  },
+  {
+    "revision": "a2e0bd79ee00b1953a6e",
+    "url": "/static/css/main.fc802260.chunk.css"
+  },
+  {
+    "revision": "338954f390a5c73ad0e2",
+    "url": "/static/js/2.5c33e2e3.chunk.js"
+  },
+  {
+    "revision": "a2e0bd79ee00b1953a6e",
+    "url": "/static/js/main.dfc24fb1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
