c2fc491c02e61fb74294b53bee6e88f79fa98319
Luke Lu <>
2019-10-04 10:46:33 +1000

    UI modified

---
+++ client/build/precache-manifest.2fc158b5a94c7799d8a7fb18944762c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f599a7c5b405a366e36c3de5cc09db16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6892b8d5eb71ab7fd4c5",
+    "url": "/static/css/2.848db5fd.chunk.css"
+  },
+  {
+    "revision": "8a838802d8f11f74d9cb",
+    "url": "/static/css/main.7b204513.chunk.css"
+  },
+  {
+    "revision": "6892b8d5eb71ab7fd4c5",
+    "url": "/static/js/2.37d86418.chunk.js"
+  },
+  {
+    "revision": "8a838802d8f11f74d9cb",
+    "url": "/static/js/main.0c5a3728.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
