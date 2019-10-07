336d2945dfe59d647280fa89e5735afc894a9ca4
Luke Lu <ailuqun313@gmail.com>
2019-10-07 16:58:42 +1100

    Version 1.0

---
+++ client/build/precache-manifest.9130e73779431422117e7b9f20f5d45a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d7d564b02944618890bce671320f5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3697b4871b228729484e",
+    "url": "/static/css/2.91d7f120.chunk.css"
+  },
+  {
+    "revision": "df7d2ddb6cabc0ad6ca4",
+    "url": "/static/css/main.48fe34d7.chunk.css"
+  },
+  {
+    "revision": "3697b4871b228729484e",
+    "url": "/static/js/2.30b875b0.chunk.js"
+  },
+  {
+    "revision": "df7d2ddb6cabc0ad6ca4",
+    "url": "/static/js/main.37440062.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
